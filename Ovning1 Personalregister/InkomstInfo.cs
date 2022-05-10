8c9d4b5e9fa527ebd371a9ae57e0a63dd39b943e
fyrka <fyrka@K413>
2022-05-10 16:44:21 +0200

    Add project files.

---
+++ Ovning1 Personalregister/InkomstInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning1_Personalregister
+{
+    internal class InkomstInfo
+    {
+        public string Namn { get; set; }    
+        public int  lon { get; set; }
+
+    }
+}
+
---
+++ Ovning1 Personalregister/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class program
+{
+
+    static void Main(string[] args)
+    {
+        InkomstInfo info = new InkomstInfo();
+
+        Console.WriteLine("Ange ditt namn:");
+        info.Namn = Console.ReadLine();
+        Console.WriteLine("Ange din lön:");
+        info.lon = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(InkomstInfo);
+
+
+    }
+}
+
